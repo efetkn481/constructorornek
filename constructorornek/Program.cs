0199a307d757b8c1945d5ce7045d06d90d693e67
efetekin481@outlook.com <efetekin481@outlook.com>
2023-03-20 23:20:57 +0300

    Add project files.

---
+++ constructorornek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string blg; 
+            Console.Write("Ad - Soyad - Yaş - Meslek = ");
+            blg = Console.ReadLine();
+            bilgiler kisi = new bilgiler(blg);
+            Console.Read();
+
+        }
+    }
+}
---
+++ constructorornek/bilgiler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructorornek
+{
+    internal class bilgiler
+    {
+        public bilgiler(string bilgi) 
+        {
+            Console.WriteLine("Bilgiler: " + bilgi);
+        }
+    }
+}
